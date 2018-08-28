398ebb42965cef69dbc04fcec92c32598f85c1d9
renildomarcio <krsolucoesweb@gmail.com>
2018-08-28 02:33:48 -0300

    Update shared.lua

--- config/shared.lua
+++ config/shared.lua
@@ -1,4 +1,4 @@
--- Global configuration file
+--- Global configuration file
 
 local Keys = {
 	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
